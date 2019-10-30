8ef86e98fd9db16eeacfe67893170557425cb0b1
Alan Macgowan <alanmacgowan@gmail.com>
2019-10-30 13:34:58 -0300

    Added APIClient and Index logic

--- FrontEnd/Pages/Index.cshtml.cs
+++ FrontEnd/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ConferenceDTO;
+using FrontEnd.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,16 +12,37 @@ namespace FrontEnd.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        protected readonly IApiClient _apiClient;
+        public IEnumerable<IGrouping<DateTimeOffset?, SessionResponse>> Sessions { get; set; }
+        public IEnumerable<(int Offset, DayOfWeek? DayofWeek)> DayOffsets { get; set; }
+        public int CurrentDayOffset { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IApiClient apiClient)
         {
-            _logger = logger;
+            _apiClient = apiClient;
         }
 
-        public void OnGet()
+        public async Task OnGet(int day = 0)
         {
+            CurrentDayOffset = day;
 
+            var sessions = await _apiClient.GetSessionsAsync();
+
+            var startDate = sessions.Min(s => s.StartTime?.Date);
+
+            var offset = 0;
+            DayOffsets = sessions.Select(s => s.StartTime?.Date)
+                                 .Distinct()
+                                 .OrderBy(d => d)
+                                 .Select(day => (offset++, day?.DayOfWeek));
+
+            var filterDate = startDate?.AddDays(day);
+
+            Sessions = sessions.Where(s => s.StartTime?.Date == filterDate)
+                               .OrderBy(s => s.TrackId)
+                               .GroupBy(s => s.StartTime)
+                               .OrderBy(g => g.Key);
         }
+
     }
 }
---
+++ FrontEnd/Services/IApiClient.cs
@@ -0,0 +1,18 @@
+﻿using ConferenceDTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Services
+{
+    public interface IApiClient
+    {
+        Task<List<SessionResponse>> GetSessionsAsync();
+        Task<SessionResponse> GetSessionAsync(int id);
+        Task<List<SpeakerResponse>> GetSpeakersAsync();
+        Task<SpeakerResponse> GetSpeakerAsync(int id);
+        Task PutSessionAsync(Session session);
+        Task<bool> AddAttendeeAsync(Attendee attendee);
+        Task<AttendeeResponse> GetAttendeeAsync(string name);
+        Task DeleteSessionAsync(int id);
+    }
+}
