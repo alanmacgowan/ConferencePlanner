bc08fc34c61144251a04e2e9f1a2e42656154d99
Alan Macgowan <alanmacgowan@gmail.com>
2019-10-29 21:38:42 -0300

    Added AttendeesController and SessionsController

--- BackEnd/Controllers/SpeakersController.cs
+++ BackEnd/Controllers/SpeakersController.cs
@@ -20,88 +20,30 @@ namespace BackEnd.Controllers
 
         // GET: api/Speakers
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Speaker>>> GetSpeakers()
+        public async Task<ActionResult<List<ConferenceDTO.SpeakerResponse>>> GetSpeakers()
         {
-            return await _context.Speakers.ToListAsync();
+            var speakers = await _context.Speakers.AsNoTracking()
+                                            .Include(s => s.SessionSpeakers)
+                                                .ThenInclude(ss => ss.Session)
+                                            .Select(s => s.MapSpeakerResponse())
+                                            .ToListAsync();
+            return speakers;
         }
 
         // GET: api/Speakers/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Speaker>> GetSpeaker(int id)
+        public async Task<ActionResult<ConferenceDTO.SpeakerResponse>> GetSpeaker(int id)
         {
-            var speaker = await _context.Speakers.FindAsync(id);
-
+            var speaker = await _context.Speakers.AsNoTracking()
+                                            .Include(s => s.SessionSpeakers)
+                                                .ThenInclude(ss => ss.Session)
+                                            .SingleOrDefaultAsync(s => s.Id == id);
             if (speaker == null)
             {
                 return NotFound();
             }
-
-            return speaker;
+            return speaker.MapSpeakerResponse();
         }
 
-        // PUT: api/Speakers/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutSpeaker(int id, Speaker speaker)
-        {
-            if (id != speaker.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(speaker).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SpeakerExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/Speakers
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<Speaker>> PostSpeaker(Speaker speaker)
-        {
-            _context.Speakers.Add(speaker);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetSpeaker", new { id = speaker.Id }, speaker);
-        }
-
-        // DELETE: api/Speakers/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Speaker>> DeleteSpeaker(int id)
-        {
-            var speaker = await _context.Speakers.FindAsync(id);
-            if (speaker == null)
-            {
-                return NotFound();
-            }
-
-            _context.Speakers.Remove(speaker);
-            await _context.SaveChangesAsync();
-
-            return speaker;
-        }
-
-        private bool SpeakerExists(int id)
-        {
-            return _context.Speakers.Any(e => e.Id == id);
-        }
     }
 }
