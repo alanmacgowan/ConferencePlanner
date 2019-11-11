445bb6c1641f3ab3fc7281e0ad2e109abaec6070
Alan Macgowan <alanmacgowan@gmail.com>
2019-11-11 12:08:28 -0300

    Added Welcome registration page for Attendees

--- FrontEnd/Areas/Identity/ClaimsPrincipalFactory.cs
+++ FrontEnd/Areas/Identity/ClaimsPrincipalFactory.cs
@@ -26,6 +26,12 @@ namespace FrontEnd.Areas.Identity
                 identity.MakeAdmin();
             }
 
+            var attendee = await _apiClient.GetAttendeeAsync(user.UserName);
+            if (attendee != null)
+            {
+                identity.MakeAttendee();
+            }
+
             return identity;
         }
     }
---
+++ FrontEnd/Pages/Welcome.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using FrontEnd.Pages.Models;
+using FrontEnd.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Pages
+{
+    public class WelcomeModel : PageModel
+    {
+        private readonly IApiClient _apiClient;
+
+        public WelcomeModel(IApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        [BindProperty]
+        public Attendee Attendee { get; set; }
+
+        public IActionResult OnGet()
+        {
+            // Redirect to home page if user is anonymous or already registered as attendee
+            var isAttendee = User.IsAttendee();
+
+            if (!User.Identity.IsAuthenticated || isAttendee)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var success = await _apiClient.AddAttendeeAsync(Attendee);
+
+            if (!success)
+            {
+                ModelState.AddModelError("", "There was an issue creating the attendee for this user.");
+                return Page();
+            }
+
+            // Re-issue the auth cookie with the new IsAttendee claim
+            User.MakeAttendee();
+            await HttpContext.SignInAsync(IdentityConstants.ApplicationScheme, User);
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
