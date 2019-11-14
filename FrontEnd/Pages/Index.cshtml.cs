e3f13b8f9de4926c4296257307dfb5b15dcfd0bb
Alan Macgowan <alanmacgowan@gmail.com>
2019-11-14 14:05:18 -0300

    Added AgendaPartial

--- FrontEnd/Pages/Index.cshtml.cs
+++ FrontEnd/Pages/Index.cshtml.cs
@@ -21,35 +21,145 @@ namespace FrontEnd.Pages
         [TempData]
         public string Message { get; set; }
         public bool ShowMessage => !string.IsNullOrEmpty(Message);
+        public List<SessionResponse> UserSessions { get; set; }
+        public ConferenceData ConferenceModel { get; private set; }
 
         public IndexModel(IApiClient apiClient)
         {
             _apiClient = apiClient;
         }
 
-        public async Task OnGet(int day = 0)
+        //public async Task OnGet(int day = 0)
+        //{
+        //    IsAdmin = User.IsAdmin();
+
+        //    CurrentDayOffset = day;
+
+        //    var sessions = await GetSessionsAsync();
+
+        //    var startDate = sessions.Min(s => s.StartTime?.Date);
+
+        //    var offset = 0;
+        //    DayOffsets = sessions.Select(s => s.StartTime?.Date)
+        //                         .Distinct()
+        //                         .OrderBy(d => d)
+        //                         .Select(day => (offset++, day?.DayOfWeek));
+
+        //    var filterDate = startDate?.AddDays(day);
+
+        //    Sessions = sessions.Where(s => s.StartTime?.Date == filterDate)
+        //                       .OrderBy(s => s.TrackId)
+        //                       .GroupBy(s => s.StartTime)
+        //                       .OrderBy(g => g.Key);
+        //}
+
+        protected virtual Task<List<SessionResponse>> GetSessionsAsync()
         {
-            IsAdmin = User.IsAdmin();
-
-            CurrentDayOffset = day;
-
-            var sessions = await _apiClient.GetSessionsAsync();
-
-            var startDate = sessions.Min(s => s.StartTime?.Date);
-
-            var offset = 0;
-            DayOffsets = sessions.Select(s => s.StartTime?.Date)
-                                 .Distinct()
-                                 .OrderBy(d => d)
-                                 .Select(day => (offset++, day?.DayOfWeek));
-
-            var filterDate = startDate?.AddDays(day);
-
-            Sessions = sessions.Where(s => s.StartTime?.Date == filterDate)
-                               .OrderBy(s => s.TrackId)
-                               .GroupBy(s => s.StartTime)
-                               .OrderBy(g => g.Key);
+            return _apiClient.GetSessionsAsync();
         }
 
+        public async Task<IActionResult> OnGetAsync(int day = 0)
+        {
+            CurrentDayOffset = day;
+
+            if (User.Identity.IsAuthenticated)
+            {
+                UserSessions = await _apiClient.GetSessionsByAttendeeAsync(User.Identity.Name);
+            }
+
+            ConferenceModel = GetConferenceDataAsync(); //await GetConferenceDataAsync();
+
+            if (CurrentDayOffset > 0 && !ConferenceModel.ContainsKey(CurrentDayOffset))
+            {
+                // Requested day is no longer valid, redirect to first day
+                return RedirectToPage();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int sessionId, int day = 0)
+        {
+            await _apiClient.AddSessionToAttendeeAsync(User.Identity.Name, sessionId);
+
+            if (day > 0)
+            {
+                return RedirectToPage(new { day });
+            }
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostRemoveAsync(int sessionId, int day = 0)
+        {
+            await _apiClient.RemoveSessionFromAttendeeAsync(User.Identity.Name, sessionId);
+
+            if (day > 0)
+            {
+                return RedirectToPage(new { day });
+            }
+
+            return RedirectToPage();
+        }
+
+        //protected virtual Task<ConferenceData> GetConferenceDataAsync()
+        //{
+        //    return _cache.GetOrCreateAsync(CacheKeys.ConferenceData, async entry =>
+        //    {
+        //        entry.SetSlidingExpiration(TimeSpan.FromHours(1));
+
+        //        var sessions = await _apiClient.GetSessionsAsync();
+        //        return GenerateConferenceData(sessions);
+        //    });
+        //}
+
+        protected virtual ConferenceData GetConferenceDataAsync()
+        {
+            var sessions = GetSessionsAsync();
+            return GenerateConferenceData(sessions.Result);
+        }
+
+        public static ConferenceData GenerateConferenceData(List<SessionResponse> sessions)
+        {
+            var startDate = sessions.Min(s => s.StartTime?.Date);
+
+            var dayOffsets = sessions.Select(s => s.StartTime?.Date)
+                                     .Distinct()
+                                     .OrderBy(d => d)
+                                     .Select(day => (Offset: (int)Math.Floor((day.Value - startDate)?.TotalDays ?? 0),
+                                                     day?.DayOfWeek))
+                                     .ToList();
+
+            var confData = new ConferenceData(dayOffsets.Count);
+
+            foreach (var day in dayOffsets)
+            {
+                var filterDate = startDate?.AddDays(day.Offset);
+
+                confData[day.Offset] = sessions.Where(s => s.StartTime?.Date == filterDate)
+                                               .OrderBy(s => s.Track.Name)
+                                               .GroupBy(s => s.StartTime)
+                                               .OrderBy(g => g.Key);
+            }
+
+            confData.StartDate = startDate;
+            confData.DayOffsets = dayOffsets;
+
+            return confData;
+        }
+
+        public class ConferenceData : Dictionary<int, IEnumerable<IGrouping<DateTimeOffset?, SessionResponse>>>
+        {
+            public ConferenceData(int capacity) : base(capacity)
+            {
+            }
+
+            public DateTimeOffset? StartDate { get; set; }
+
+            public DateTimeOffset? EndDate { get; set; }
+
+            public IEnumerable<(int Offset, DayOfWeek? DayofWeek)> DayOffsets { get; set; }
+        }
     }
+
 }
---
+++ FrontEnd/Pages/MyAgenda.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferenceDTO;
+using FrontEnd.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontEnd.Pages
+{
+    [Authorize]
+    public class MyAgendaModel : IndexModel
+    {
+        public MyAgendaModel(IApiClient client)
+            : base(client)
+        {
+
+        }
+
+        protected override Task<List<SessionResponse>> GetSessionsAsync()
+        {
+            return _apiClient.GetSessionsByAttendeeAsync(User.Identity.Name);
+        }
+    }
+}
